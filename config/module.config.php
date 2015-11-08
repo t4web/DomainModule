843dda82f547054e07197a7647cc336a8fb65d88
maxgu <maxgu@t4web.com.ua>
2015-11-08 10:10:47 +0200

    update config

--- config/module.config.php
+++ config/module.config.php
@@ -3,13 +3,7 @@
 return [
     'service_manager' => [
         'abstract_factories' => [
-            'T4webBase\Module\ConfigAbstractFactory',
-            'T4webBase\Domain\Factory\EntityAbstractFactory',
-            'T4webBase\Domain\Mapper\DbMapperAbstractFactory',
-            'T4webBase\Domain\Repository\DbRepositoryAbstractFactory',
-            'T4webBase\Db\TableGatewayAbstractFactory',
-            'T4webBase\Db\TableAbstractFactory',
-            'T4webBase\Domain\Criteria\CriteriaFactoryAbstractFactory',
+            'T4web\DomainModule\Service\CreatorAbstractFactory',
         ],
     ],
 ];
