7d899f7a43b1dd7a6f269df03abfeb58ae515000
maxgu <gulturyan@t4web.com.ua>
2015-11-11 09:29:24 +0200

    add onBootstrap exception "T4web\DomainModule has conflict with T4webBase module, for use it you must disable T4webBase module."

--- src/Module.php
+++ src/Module.php
@@ -3,9 +3,28 @@ namespace T4web\DomainModule;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\ModuleManager;
+use \RuntimeException;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface, BootstrapListenerInterface
 {
+    public function onBootstrap(EventInterface $e)
+    {
+        /** @var ModuleManager $moduleManager */
+        $moduleManager = $e->getApplication()
+            ->getServiceManager()
+            ->get('ModuleManager');
+
+        $baseModule = $moduleManager->getModule('T4webBase');
+
+        if (!is_null($baseModule)) {
+            throw new RuntimeException('T4web\DomainModule has conflict with T4webBase module, for use it you must disable T4webBase module.');
+        }
+
+    }
+
     public function getConfig($env = null)
     {
         return include dirname(__DIR__) . '/config/module.config.php';
